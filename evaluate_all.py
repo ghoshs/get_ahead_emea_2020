39620b08e1bb16c426dbf0d73d1781b4bc58dd23
Shrestha Ghosh <shrestha@lap-15-89.mpi-inf.mpg.de>
2020-07-21 17:46:58 +0200

    one evaluation script to be called

---
+++ evaluate_all.py
@@ -0,0 +1,11 @@
+from evaluate_flattenedIterator import evaluate_flattenedIterator as evaluateWeek1
+from evaluate_balancedParenthesis import evaluate_balancedParenthesis as evaluateWeek2
+
+def evaluate_all():
+	print('Evaluating Week 1 ..')
+	evaluateWeek1()
+	print('Evaluating Week 2 ..')
+	evaluateWeek2()
+
+if __name__ == '__main__':
+	evaluate_all()
